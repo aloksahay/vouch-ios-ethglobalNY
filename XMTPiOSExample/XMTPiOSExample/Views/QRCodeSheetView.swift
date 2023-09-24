c5d9cce5a9ffc21fea061243d0aa4a735822acc7
Steve Smith <stevysmith.ss@gmail.com>
2023-09-24 00:43:44 -0400

    Add logo and splash

--- XMTPiOSExample/XMTPiOSExample/Views/QRCodeSheetView.swift
+++ XMTPiOSExample/XMTPiOSExample/Views/QRCodeSheetView.swift
@@ -11,10 +11,17 @@ struct QRCodeSheetView: View {
 	var image: UIImage?
 
     var body: some View {
-        Image(uiImage: UIImage(named: "qrimage")!) // Display the image using Image(uiImage:)
-            .resizable()
-            .aspectRatio(contentMode: .fit)
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .padding()
+        VStack {
+            Text("Scan the below QR code to get started and to connect your wallet with our network")
+                .font(.headline)
+                .foregroundColor(.black)
+                .padding(.top, 20)
+            
+            Image(uiImage: UIImage(named: "qrimage")!)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .padding()
+        }
     }
 }
